f2d607dc45ef0b409cdcf68df486cc48e12880c0
Yairzdr <63109080+Yairzdr@users.noreply.github.com>
2020-04-04 11:17:46 +0300

    test1

--- Warmup.java
+++ Warmup.java
@@ -1,6 +1,9 @@
 public class Warmup {
 	//This function is searching a key in a given unsorted array by moving 'bk' steps back every 'fd' steps.
-    public static int backtrackingSearch(int[] arr, int x, int fd, int bk, Stack myStack) {
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+	public static int backtrackingSearch(int[] arr, int x, int fd, int bk, Stack myStack) {
     	int i=0;
     	int fdCounter=0;
         while(i<arr.length)
@@ -13,7 +16,7 @@ public class Warmup {
         		i=(int) myStack.pop();     		
         		fdCounter=0;
         	}
-        	elses
+        	else
         	{
         		myStack.push(i);
         		fdCounter++;
@@ -23,9 +26,9 @@ public class Warmup {
     	return -1;
     }
 
-    public static int consistentBinSearch(int[] arr, int x, Stack myStack) {
+ //   public static int consistentBinSearch(int[] arr, int x, Stack myStack) {
         // TODO: implement your code here
-    }
+ //   }
 
     private static int isConsistent(int[] arr) {
         double res = Math.random() * 100 - 75;
